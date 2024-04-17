31649a9659d8ee231ff5d5a69646b6ca10fbe001
Alexis-Benamar <alexis.benamar@viacesi.fr>
2024-04-17 11:39:28 +0200

    add neck & head movements + update README.md

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # bug lift
 
-a silly little idea based on a silly little guy with an incredible neck
+> _look at him go!_
+
+A silly little [p5.js](https://p5js.org/) project based on this silly little guy with an incredible neck. Now interactive!
+
+<img src="https://github.com/Alexis-Benamar/bug-lift/blob/main/images/background.webp?raw=true" width=400 />
+
+## How to run locally
+
+First, clone the project on your pc.
+
+You will need `node` & `browser-sync` installed globally in order to run the project as a web server (otherwise, images won't be loaded in the browser)
+```
+npm i -g browser-sync
+```
+
+Then, go inside the project's folder & run it with the following command:
+```
+browser-sync start --server -f -w --no-open
+```
--- sketch.js
+++ sketch.js
@@ -1,16 +1,91 @@
+const NECK_OFFSET_X = 0.357
+const NECK_OFFSET_Y = 0.605
+const DEFAULT_NECK_LENGTH = 504
+const DEFAULT_NECK_ANGLE = -40
+const ROTATION_RATE = 0.5
+const TRANSLATION_RATE = 2
+
 let bg
+
+let neck
+let neckX
+let neckY
+let neckVector
+let neckLength = DEFAULT_NECK_LENGTH
+const neckWidth = 31
+let neckAngle = DEFAULT_NECK_ANGLE
+
+let head
+const headLength = 213
+const headHeight = 159
+// let headAngle = neckAngle + 40
+
 const getCanvasHeight = () => windowWidth >= windowHeight * 1.5 ? windowHeight : windowWidth * (2/3)
 const getCanvasWidth = () => windowWidth >= windowHeight * 1.5 ? windowHeight * 1.5 : windowWidth
 
 function setup() {
   bg = loadImage('./images/background.jpg')
+  neck = loadImage('./images/neck.png')
+  head = loadImage('./images/head.png')
+
   createCanvas(getCanvasWidth(), getCanvasHeight())
+  angleMode(DEGREES);
+
+  neckX = width * NECK_OFFSET_X
+  neckY = height * NECK_OFFSET_Y
+  neckVector = createVector(neckLength, 0)
+  neckVector.rotate(neckAngle)
 }
 
 function windowResized() {
   resizeCanvas(getCanvasWidth(), getCanvasHeight())
+  neckX = width * NECK_OFFSET_X
+  neckY = height * NECK_OFFSET_Y
+}
+
+function keyPressed() {
+  // Reset neck & head pos by pressing 'R'
+  if (key == 'r') {
+    translate(neckX, neckY)
+    neckAngle = DEFAULT_NECK_ANGLE
+    neckVector.x = neckLength
+    neckVector.y = 0
+    neckVector.rotate(neckAngle)
+  }
 }
 
 function draw() {
+  if (keyIsDown(RIGHT_ARROW)) {
+    neckAngle += ROTATION_RATE
+    neckVector.rotate(ROTATION_RATE)
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    neckAngle -= ROTATION_RATE
+    neckVector.rotate(-ROTATION_RATE)
+  }
+  if (keyIsDown(UP_ARROW)) {
+    neckLength += TRANSLATION_RATE
+    neckVector.setMag(neckLength)
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    neckLength -= TRANSLATION_RATE
+    neckVector.setMag(neckLength)
+  }
+
+  // Draw background & move origin to neck base
   background(bg)
+  translate(neckX, neckY)
+
+  // Draw neck at neckAngle
+  push()
+  rotate(neckAngle)
+  image(neck, 0, -neckWidth/2, neckLength, neckWidth)
+  pop()
+
+  // Draw head on tip of neck
+  push()
+  translate(neckVector.x, neckVector.y)
+  // rotate(headAngle)
+  image(head, -headLength*0.05, -headHeight*0.88, headLength, headHeight)
+  pop()
 }
